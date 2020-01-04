cd1bd1d8b4276808593f59e163d87920a664f90e
eunsangzz <48229728+eunsangzz@users.noreply.github.com>
2020-01-04 18:28:38 +0900

    first commit

---
+++ playermovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+
+    public float movePower = 1f;
+    public float jumpPower = 1f;
+
+    Rigidbody2D rigid;
+    Animator animator;
+    
+
+    Vector3 movement;
+    bool isJumping = false;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        rigid = gameObject.GetComponent<Rigidbody2D> ();
+        animator = gameObject.GetComponentInChildren<Animator>();
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxisRaw ("Horizontal")< 0)
+        {
+            animator.SetInteger("Direction", -1);
+        }
+        if (Input.GetAxisRaw ("Horizontal") > 0)
+        {
+            animator.SetInteger("Direction", 1);
+        }
+		if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+        if (Input.GetButtonDown("Jump")&& !animator.GetBool("isJumping"))
+        {
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+            animator.SetTrigger("doJumping");
+        }
+	}
+
+    void FixedUpdate()
+    {
+        Move ();
+        Jump ();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        
+        if (Input.GetAxisRaw ("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+
+            transform.localScale = new Vector3(-4, 4, 1);
+        }
+
+        else if(Input.GetAxisRaw ("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+
+            transform.localScale = new Vector3(4, 4, 1);
+        }
+
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+            return;
+
+        rigid.velocity = Vector2.zero;
+
+        Vector2 jumpvelocity = new Vector2(0, jumpPower);
+        rigid.AddForce(jumpvelocity, ForceMode2D.Impulse);
+
+        isJumping = false;
+    }
+}
