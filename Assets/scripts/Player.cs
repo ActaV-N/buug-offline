984723fe3383e70d7cc2a2aa232126823367c610
JunL <gksdmltk02@gmail.com>
2020-01-05 13:18:49 +0900

    README.md 완성

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Linear attack
+    // Groun attack
+    // Drop attack
+    string pattern = "Linear"; // default attack pattern(Linear attack)
+
+    // None
+    // Fire
+    // Ice
+    // Thunder
+    string attribute = "none";
+
+
+    public GameObject weapon;
+
+    public float horizontalSpeed = 1f;
+    public float jumpForce = 1f;
+
+    bool isAttack = false;
+     
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        weapon.transform.position = transform.position + new Vector3(-1, 1);
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            if (!isAttack)
+            {
+                weapon.transform.position = transform.position + new Vector3(0.3f, 0.3f);
+            }
+            transform.position += new Vector3(-horizontalSpeed * Time.deltaTime, 0);
+        }
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            if (!isAttack)
+            {
+                weapon.transform.position = transform.position + new Vector3(-0.3f, 0.3f);
+            }
+            transform.position += new Vector3(horizontalSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            print("fire");
+        }
+    }
+}
